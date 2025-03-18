922ba618d6d97c940811d810bd4db9aa7a054e64
thenahraf <farhan.ug23@nsut.ac.in>
2025-03-18 18:07:11 +0530

    make function of posts

--- convex/posts.ts
+++ convex/posts.ts
@@ -11,17 +11,3 @@ export const generateUploadUrl = mutation(async(ctx)=>{
 });
 
 
-
-export const createPost = mutation({
-    args:{
-        caption: v.optional(v.string()),
-        storageId: v.id("_storage"),
-    },
-    handler: async(ctx, args)=>{
-    const identity = ctx.auth.getUserIdentity();
-    if(!identity){
-        throw new Error("Unautorized")
-    }
-
- 
-})
