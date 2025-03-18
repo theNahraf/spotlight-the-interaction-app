04535c45da5c0ad3cd31a2886545addb0acf40d1
thenahraf <farhan.ug23@nsut.ac.in>
2025-03-18 18:12:21 +0530

    writing post function for create post in convex

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,13 +1,2 @@
-import { v } from "convex/values";
-import { mutation } from "./_generated/server";
-
-//
-export const generateUploadUrl = mutation(async(ctx)=>{
-    const identity = ctx.auth.getUserIdentity();
-    if(!identity){
-        throw new Error("Unautorized")
-    }
-    return await ctx.storage.generateUploadUrl();
-});
 
 
